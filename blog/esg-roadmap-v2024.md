d54efdb58b5d04d7b7f47deafabfc33d4f4c6303
tth37 <xgpsthd0902@outlook.com>
2024-04-15 17:02:57 +0800

    Add blog post page

---
+++ blog/esg-roadmap-v2024.md
@@ -0,0 +1,50 @@
+---
+title: "ESG Roadmap v2024"
+date: 2024-04-15
+author: Mengyang Liu
+---
+
+## 0. 学会提问
+
+学会积极主动地去提问，在任何场合，对任何对象进行提问。Active 的态度是好学品质最好的体现。但是提问是一门艺术，不是所有问题都应该问，提问是有技巧的。
+
+- 《提问的艺术》：[http://www.catb.org/~esr/faqs/smart-questions.html](http://www.catb.org/~esr/faqs/smart-questions.html)
+- 《提问的艺术》中文版：[https://lug.ustc.edu.cn/wiki/doc/smart-questions/](https://lug.ustc.edu.cn/wiki/doc/smart-questions/)
+
+## 1. 基础知识
+
+- 编程基础
+  - C/C++：《C++ Primer》
+  - Python：网上的在线资料足够了，推荐 [W3Schools 教程](https://www.w3schools.com/python/default.asp) 或者 [Python官方教程](https://docs.python.org/3/tutorial/index.html)
+  - 其他新兴语言比如 Go 或 Rust，如果你有兴趣，可以学一学
+  - 算法：我个人还是喜欢《算法导论》的，当然也可以看看网上有很多别人写的简化版的算法入门内容，推荐 [OI Wiki](https://oi-wiki.org/)
+- CS 基础
+  - CS 必备工具：[MIT Missing Semester](https://missing.csail.mit.edu/)，[IPADS 新人培训](https://space.bilibili.com/1085720801/channel/collectiondetail?sid=78079)
+  - 计算机体系结构：《CSAPP》
+- DL基础
+  - 深度学习现在太重要了，我觉得学习深度学习的基础是一个性价比很高的事情，就算你以后不准备研究 AI，我也推荐学习一下深度学习的基础知识。
+  - 李沐《动手学深度学习》：[https://space.bilibili.com/1567748478/channel/seriesdetail?sid=358497](https://space.bilibili.com/1567748478/channel/seriesdetail?sid=358497)
+
+## 2. 步入科研
+
+- MIT《学术道德》：[http://integrity.mit.edu/](http://integrity.mit.edu/)
+- 如何找论文？
+  - DBLP
+  - Google Scholar
+  - arXiv
+  - 一些官方数据库：IEEE，ACM 等
+- 读什么论文？
+  - CCF推荐列表，A类会议为上佳：[https://www.ccf.org.cn/Academic_Evaluation/By_category/](https://www.ccf.org.cn/Academic_Evaluation/By_category/)
+  - TH-CPL推荐列表，A类会议为上佳：[https://numbda.cs.tsinghua.edu.cn/~yuwj/TH-CPL.pdf](https://numbda.cs.tsinghua.edu.cn/~yuwj/TH-CPL.pdf)
+  - 为什么 CS 领域会议比期刊的影响力更高？因为 CS 领域迭代速度很快，期刊周期太长，最新的优秀成果大多会第一时间发表于会议。
+- 如何读论文？
+  - Keshav《How to Read a Paper》：[https://dl.acm.org/doi/10.1145/1273445.1273458](https://dl.acm.org/doi/10.1145/1273445.1273458)
+- 科研工具
+  - LaTeX
+  - 论文管理工具，不要用文件夹管理论文了
+    - 我个人用的 Zotero
+    - 有人推荐 ReadPaper，我没深入使用过
+
+## 3. 靠自己
+
+我觉得本科生最重要的品质是积极主动（active and self-motivated），主动提问，主动思考，主动行动。也许你不用马上做出创新，但是你应该时刻在思考，时刻抱着想要创新的目标。如果你有志于科研事业，本科四年是打好科研基础最为关键的四年，如何在这有限的时间里最大化收益绝对是你个人发展所要考虑的头号问题。
--- build.js
+++ build.js
@@ -3,17 +3,42 @@ const minify = require('html-minifier').minify;
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
+const { marked } = require('marked');
+
+function render(md) {
+  return marked(md);
+}
 
 // Read the configuration file
 const global = yaml.load(fs.readFileSync(path.join(__dirname, 'configs/global.yaml'), 'utf8'));
 const schedules = yaml.load(fs.readFileSync(path.join(__dirname, 'configs/schedules.yaml'), 'utf8'));
+
+// Read the blog posts from /blog, sort by date
+const blogs = fs.readdirSync(path.join(__dirname, 'blog'))
+  .map(file => {
+    const content = fs.readFileSync(path.join(__dirname, 'blog', file), 'utf8');
+    const [_, frontMatter, ...rest] = content.split('---');
+    const { title, date, author } = yaml.load(frontMatter);
+    const html = render(rest.join('\n'));
+    return { title, date, author, html, id: file.replace('.md', '') };
+  })
+  .sort((a, b) => new Date(b.date) - new Date(a.date))
+  .map((blog, i) => ({ ...blog, date: new Date(blog.date).toLocaleDateString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit' }) }));
 
 // Render global.upcoming
 latest = schedules[0];
 global.upcoming = `${latest.presenter} will present a/an ${latest.conf} paper on ${latest.time}, ${latest.date} at ${latest.location}.`;
 
 // Render index.ejs
-const index = minify(ejs.render(fs.readFileSync(path.join(__dirname, 'index.ejs'), 'utf8'), { global, schedules }), {
+const index = minify(ejs.render(fs.readFileSync(path.join(__dirname, 'views', 'index.ejs'), 'utf8'), { global, blogs, schedules }), {
+  collapseWhitespace: true,
+  removeComments: true,
+  minifyJS: true,
+  minifyCSS: true
+});
+
+// Render blog.ejs
+const blog = minify(ejs.render(fs.readFileSync(path.join(__dirname, 'views', 'blog.ejs'), 'utf8'), { global, blogs }), {
   collapseWhitespace: true,
   removeComments: true,
   minifyJS: true,
@@ -22,3 +47,4 @@ const index = minify(ejs.render(fs.readFileSync(path.join(__dirname, 'index.ejs'
 
 // Write the rendered HTML to index.html
 fs.writeFileSync(path.join(__dirname, 'index.html'), index);
+fs.writeFileSync(path.join(__dirname, 'blog.html'), blog);
